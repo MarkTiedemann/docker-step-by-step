254b275e1af050787febf4bad71a5a74a1afebb3
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-10-24 05:13:11 +0200

    Concat numerically sorted

--- concat.js
+++ concat.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
+const path = require('path')
 
 process.chdir('slides')
 
+const basename = f => path.basename(f, path.extname(f))
+
 const files = fs
   .readdirSync('.')
+  .sort((a, b) => Number(basename(a)) - Number(basename(b)))
   .map(f => fs.readFileSync(f))
   .map(b => b.toString())
 
